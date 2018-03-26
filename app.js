cf61166b2d38f4f88ffb37e05c9cfd43752ba774
Benoît Delamea <Benoit@Benoits-MacBook-Pro.local>
2018-03-26 15:16:57 +0200

    final release 03-2018

--- app.js
+++ app.js
@@ -1,14 +1,108 @@
 'use strict'
 
+window.$ = window.jQuery = require('jquery');
 const nodemailer = require('nodemailer');
 var count = 0;
 var data = new Array();
-var time = '';
+var config = new Array();
+var navettesNumber = 0;
 
-var mailerTool = function() {
+function whatDisplay() {
+    document.getElementById("subform1").classList.remove('d-none');
+    document.getElementById("subform2").classList.remove('d-none');
+    document.getElementById("subform3").classList.remove('d-none');
+    document.getElementById("subform4").classList.remove('d-none');
+    document.getElementById("subform5").classList.remove('d-none');
+    document.getElementById("subform6").classList.remove('d-none');
+    document.getElementsByClassName("subinput1").required = false;
+    document.getElementsByClassName("subinput2").required = false;
+    document.getElementsByClassName("subinput3").required = false;
+    document.getElementsByClassName("subinput4").required = false;
+    document.getElementsByClassName("subinput5").required = false;
+    document.getElementsByClassName("subinput6").required = false;
+    switch (parseInt(document.getElementById('navettesNumber').value)) {
+        case 1:
+        document.getElementById("subform2").classList.add('d-none');
+        document.getElementById("subform3").classList.add('d-none');
+        document.getElementById("subform4").classList.add('d-none');
+        document.getElementById("subform5").classList.add('d-none');
+        document.getElementById("subform6").classList.add('d-none');
+        document.getElementById('emailcontent').style.marginTop = '-60px';
+        document.getElementsByClassName("subinput1").required = true;
+        navettesNumber = 1;
+        break;
+        case 2:
+        document.getElementById("subform3").classList.add('d-none');
+        document.getElementById("subform4").classList.add('d-none');
+        document.getElementById("subform5").classList.add('d-none');
+        document.getElementById("subform6").classList.add('d-none');
+        document.getElementById('emailcontent').style.marginTop = '-60px';
+        document.getElementsByClassName("subinput1").required = true;
+        document.getElementsByClassName("subinput2").required = true;
+        navettesNumber = 2;
+        break;
+        case 3:
+        document.getElementById("subform4").classList.add('d-none');
+        document.getElementById("subform5").classList.add('d-none');
+        document.getElementById("subform6").classList.add('d-none');
+        document.getElementById('emailcontent').style.marginTop = '0px';
+        document.getElementsByClassName("subinput1").required = true;
+        document.getElementsByClassName("subinput2").required = true;
+        document.getElementsByClassName("subinput3").required = true;
+        navettesNumber = 3;
+        break;
+        case 4:
+        document.getElementById("subform5").classList.add('d-none');
+        document.getElementById("subform6").classList.add('d-none');
+        document.getElementById('emailcontent').style.marginTop = '0px';
+        document.getElementsByClassName("subinput1").required = true;
+        document.getElementsByClassName("subinput2").required = true;
+        document.getElementsByClassName("subinput3").required = true;
+        document.getElementsByClassName("subinput4").required = true;
+        navettesNumber = 4;
+        break;
+        case 5:
+        document.getElementById("subform6").classList.add('d-none');
+        document.getElementById('emailcontent').style.marginTop = '0px';
+        document.getElementsByClassName("subinput1").required = true;
+        document.getElementsByClassName("subinput2").required = true;
+        document.getElementsByClassName("subinput3").required = true;
+        document.getElementsByClassName("subinput4").required = true;
+        document.getElementsByClassName("subinput5").required = true;
+        navettesNumber = 5;
+        break;
+        case 6:
+        document.getElementById('emailcontent').style.marginTop = '0px';
+        document.getElementsByClassName("subinput1").required = true;
+        document.getElementsByClassName("subinput2").required = true;
+        document.getElementsByClassName("subinput3").required = true;
+        document.getElementsByClassName("subinput4").required = true;
+        document.getElementsByClassName("subinput5").required = true;
+        document.getElementsByClassName("subinput6").required = true;
+        navettesNumber = 6;
+        break;
+        default:
+        document.getElementById("subform2").classList.add('d-none');
+        document.getElementById("subform3").classList.add('d-none');
+        document.getElementById("subform4").classList.add('d-none');
+        document.getElementById("subform5").classList.add('d-none');
+        document.getElementById("subform6").classList.add('d-none');
+        document.getElementsByClassName("form-control").required = false;
+        document.getElementsByClassName("subinput1").required = true;
+        navettesNumber = 1;
+    }
+};
+
+function mailerTool() {
 
     console.log('############# LAUNCHED #############');
 
+    count = 0;
+    data = new Array();
+    config = new Array();
+    navettesNumber = 0;
+
+    whatDisplay();
     checkFormValidity();
 
     function checkFormValidity() {
@@ -17,118 +111,126 @@ var mailerTool = function() {
             if (form.checkValidity() === false) {
                 event.preventDefault();
                 event.stopPropagation();
+                console.log('error');
             } else {
-                dataTreat(document.getElementById('inputEmails').value, document.getElementById('inputNames').value, document.getElementById('inputTime').value);
+                dataTreat();
             }
             form.classList.add('was-validated');
         }, false);
     }
 
-    function dataTreat(addresses, names, timing) {
-        count = addresses.split(';').length; // -1 to have the correct ';' count, here it's just the number of people
-        time = timing;
-        console.log(count + ' entries');
-        console.log('### Proceeding entries');
-        for (var i = 0; i < count; i++) {
-            var n = addresses.indexOf(';');
-            var t = names.indexOf(';');
-            if (n == -1 || t == -1) {
-                n = addresses.length + 1;
-                t = names.length + 1;
-            }
-            data[i] = {
-                address: addresses.substring(0, n),
-                name: names.substring(0, t)
+    function dataTreat() {
+        console.log('### Creating configuration variable with ' + navettesNumber + ' buses');
+        for (var i = 0; i < navettesNumber; i++) {
+            config[i] = {
+                addresses: document.getElementById('inputEmails' + (i + 1)).value,
+                names: document.getElementById('inputNames' + (i + 1)).value,
+                time: document.getElementById('inputTime' + (i + 1)).value
             };
-            addresses = addresses.substring(n + 1);
-            names = names.substring(t + 1);
-            if (data.length == count) {
-                sendMail(data,time, count);
+            if (i == navettesNumber - 1) {
+                setTimeout(function() {
+                    console.log(config);
+                    createData(config);
+                }, 1000);
             }
         }
+
+        function createData(config) {
+            console.log('### Proceeding entries');
+            for (var i = 0; i < config.length; i++) {
+                var subcount = config[i].addresses.split(';').length;
+                console.log(subcount + ' entries for the ' + config[i].time + ' bus');
+                count = count + subcount;
+                for (var j = 0; j < subcount; j++) {
+                    var nbadd = config[i].addresses.indexOf(';');
+                    var nbnames = config[i].names.indexOf(';');
+                    if (nbadd == -1 || nbames == -1) {
+                        data[data.length] = {
+                            address: config[i].addresses.substring(0, nbadd + 1),
+                            name: config[i].names.substring(0, nbnames + 1),
+                            time: config[i].time
+                        };
+                    } else {
+                        data[data.length] = {
+                            address: config[i].addresses.substring(0, nbadd),
+                            name: config[i].names.substring(0, nbames),
+                            time: config[i].time
+                        };
+                    }
+                    console.log('Entry n°' + data.length + ' treated');
+                    config[i].addresses = config[i].addresses.substring(nbadd + 1);
+                    config[i].names = config[i].names.substring(nbames + 1);
+
+                    if (data.length == count && i == config.length - 1) {
+                        console.log('Total entries: ' + count);
+                        sendMail();
+                    }
+                }
+            }
+        };
     }
 
-    function sendMail(data, time, count) {
-        nodemailer.createTestAccount((err, account) => {
-            let transporter = nodemailer.createTransport({
-                host: 'smtp.office365.com',
-                port: 587,
-                secure: false,
-                auth: {
-                    user: 'pnp@em-lyon.com',
-                    pass: 'Access2010'
-                }
-            });
-
-            console.log('### Sending mails');
-
-            var i = 0;
-
-            function myLoop () {           //  create a loop function
-                setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-                    var htmlSubject = 'Ton horaire de navette pour ce soir';
-                    var htmlBody = '<p>Salut à toi ' + data[i].name + ' !</p><p>La navette qui t\’a été attribuée au shotgun est celle de <strong>' + time + '</strong></p><p>Ce bus magique t\'emmènera vers une toute nouvelle salle pour la SAT BDE x JE.</p><p>On t\’attend <span style="font-size: 25px">15 minutes</span> avant le départ de ta navette pour toujours plus de kiffe.</p><p>A ce soir !</p><img src="cid:killBeerImage" alt=""><br>';
-
-                    let mailOptions = {
-                        from: '"Plug\'N\'Play" <pnp@em-lyon.com>', // sender address
-                        to: data[i].address, // list of receivers
-                        subject: htmlSubject, // Subject line
-                        // subject: document.getElementById('htmlSubject').value,
-                        // html: document.getElementById('htmlBody').value
-                        html: htmlBody, // html body
-                        attachments: [{
-                            filename: 'image.png',
-                            path: './imgs/image.png',
-                            cid: 'killBeerImage' //same cid value as in the html img src
-                        }]
-                    };
-
-                    transporter.sendMail(mailOptions, function(error, info){
-                        if(error){
-                            console.log('!!! Erreur lors de l\'envoi à ' + mailOptions.to + '.\nDétails de l\'erreur : ' + error);
-                        } else {
-                            console.log('Message n°' + (i + 1) + ' à ' + data[i].name);
-                            if (i == data.length -1) {
-                                return console.log('############# DONE #############');
-                            }
-                        }
-                    });
-                    i++;                     //  increment the counter
-                    if (i < count) {            //  if the counter < 10, call the loop function
-                        myLoop();             //  ..  again which will trigger another
-                    }                        //  ..  setTimeout()
-                }, 3000)
-            };
-            myLoop();
-            // for (var i = 0; i < data.length; i++) {
-            //
-            //     var htmlSubject = 'Ton horaire de navette pour ce soir';
-            //     var htmlBody = '<p>Salut à toi ' + data[i].name + ' !</p><p>La navette qui t\’a été attribuée au shotgun est celle de <strong>' + time + '</strong></p><p>Ce bus magique t\'emmènera vers une toute nouvelle salle pour la SAT BDE x JE.</p><p>On t\’attend <span style="font-size: 25px">15 minutes</span> avant le départ de ta navette pour toujours plus de kiffe.</p><p>A ce soir !</p><img src="cid:killBeerImage" alt=""><br>';
-            //
-            //     let mailOptions = {
-            //         from: '"Plug\'N\'Play" <lucas.lion.12@neoma-bs.com>', // sender address
-            //         to: data[i].address, // list of receivers
-            //         subject: htmlSubject, // Subject line
-            //         // subject: document.getElementById('htmlSubject').value,
-            //         // html: document.getElementById('htmlBody').value
-            //         html: htmlBody, // html body
-            //         attachments: [{
-            //             filename: 'image.png',
-            //             path: './imgs/image.png',
-            //             cid: 'killBeerImage' //same cid value as in the html img src
-            //         }]
-            //     };
-            //
-            //     transporter.sendMail(mailOptions, function(error, info){
-            //         if(error){
-            //             console.log('!!! Erreur lors de l\'envoi à ' + mailOptions.to + '.\nDétails de l\'erreur : ' + error);
-            //         }
-            //         console.log('Message n°: ' + (i + 1) + ' à ' + data[i].name);
-            //         if (i == data.length -1) {
-            //             return console.log('############# DONE');
-            //         }
-            //     });
-            // }
+    function sendMail() {
+        console.log(data);
+        let transporter = nodemailer.createTransport({
+            pool: true,
+            maxConnections: 10,
+            maxMessages: 500,
+            rateDelta: 2000,
+            rateLimit: 1,
+            host: 'smtp.office365.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'pnp@em-lyon.com',
+                pass: 'Access2010'
+            },
         });
+
+        console.log('### Sending mails');
+        var i = 0;
+
+        function myLoop () {           //  create a loop function
+            setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+                var htmlSubject = document.getElementById('htmlSubject').value.replace('§', data[i].name);
+                var htmlBody = document.getElementById('htmlBody').value.replace('§', data[i].name).replace('°', data[i].time);
+                var image = {
+                    filename: 'image.png',
+                    path: './imgs/image.png',
+                    cid: 'killBeerImage'
+                };
+
+                let mailOptions = {
+                    from: from, // sender address
+                    to: data[i].address, // list of receivers
+                    subject: htmlSubject, // Subject line
+                    html: htmlBody, // html body ==> <img src="cid:leciddonneàl'image"/>
+                    attachments: image // A BIEN LIER AVEC LE HTML
+                };
+
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log('!!! Error while sending email to ' + mailOptions.to + '.\n' + error);
+                        if (i == data.length -1) {
+                            console.log('############# DONE #############');
+                        }
+                        i++;                     //  increment the counter
+                        if (i < count) {            //  if the counter < i, call the loop function
+                            myLoop();             //  ..  again which will trigger another
+                        }
+                    } else {
+                        console.log('Email n°' + (i + 1) + ' to ' + data[i].name + ' sent');
+                        if (i == data.length - 1) {
+                            console.log('############# DONE #############');
+                        }
+                        i++;                     //  increment the counter
+                        if (i < count) {            //  if the counter < i, call the loop function
+                            myLoop();             //  ..  again which will trigger another
+                        }
+                    }
+                });
+            }, 1500)
+        };
+        myLoop();
     }
 }
